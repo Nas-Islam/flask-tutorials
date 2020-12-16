75b6e17a287d0499774dffd6d90869c4912cb893
Nas-Islam <islam626@hotmail.co.uk>
2020-12-16 14:16:00 +0000

    Updated files

--- to-do-list/application/routes.py
+++ to-do-list/application/routes.py
@@ -8,14 +8,14 @@ from flask import Flask, render_template, request, redirect, url_for
 def home():
     return render_template('home.html', all_tasks = Tasks.query.all())
 
-@app.route('/delete/<int:id>', methods=['GET', 'POST'])
-def deleted(id):
+@app.route('/delete/<int:id>')
+def delete(id):
     task_to_delete = Tasks.query.filter_by(id=id).first()
     db.session.delete(task_to_delete)
     db.session.commit()
     return redirect(url_for("home"))
 
-@app.route('/isitcomplete/<int:id>', methods=['GET', 'POST'])
+@app.route('/isitcomplete/<int:id>')
 def isitcomplete(id):
     change_completion = Tasks.query.filter_by(id=id).first()
     if change_completion.completion == "No":
@@ -28,7 +28,7 @@ def isitcomplete(id):
         return redirect(url_for("home"))
 
 @app.route('/add', methods=['GET', 'POST'])
-def addnew():
+def add():
     error=""
     form = TaskForm()
 
@@ -65,31 +65,8 @@ def updatedesc():
 
     return render_template('form.html', form = form, message = error)
 
-@app.route('/read')
-def read():
-    all_tasks = Tasks.query.all()
-    tasks_string = ""
-    title_string = "Name - Description - Completed?"
-    for task in all_tasks:
-        tasks_string += "<br>"+ task.name + " - " + task.description + " - " + task.completion
-    return title_string + tasks_string
-
-
 @app.route('/completed')
 def completed():
     return render_template('completed.html', completed_tasks = Tasks.query.filter_by(completion="Yes"))
 
 
-@app.route('/incomplete/<task_name>')
-def incomplete(task_name):
-    incompleted_task = Tasks.query.filter_by(name=task_name).first()
-    incompleted_task.completion = "No"
-    db.session.commit()
-    return "The task is incomplete"
-
-@app.route('/complete/<task_name>')
-def complete(task_name):
-    completed_task = Tasks.query.filter_by(name=task_name).first()
-    completed_task.completion = "Yes"
-    db.session.commit()
-    return redirect(url_for("home"))
